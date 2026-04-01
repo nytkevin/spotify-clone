38a2165303e660b27b5c0acb4f7816c2403b6c92
nytkevin <nzayiturikikevin@gmail.com>
2026-04-01 17:54:07 -0400

    style: Made sidebar responsive

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { useState } from "react";
+import { TiThMenu } from "react-icons/ti";
 export default function Sidebar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <aside className="flex flex-row h-screen bg-green-500 w-60 rounded-r-2xl">
+      <div className="md:hidden fixed top-4 left-4 z-50">
+        <button onClick={() => setOpen(!open)}>
+          <TiThMenu size={28} />
+        </button>
+      </div>
+      <aside
+        className={`fixed top-0 left-0 h-screen bg-green-500 w-60 rounded-r-2xl transform transition-transform duration-300
+    ${open ? "translate-x-0" : "-translate-x-full"} md:translate-x-0`}
+      >
         <div className=" space-x-2">
           <button className="bg-blue-400 rounded-2xl w-16">playlist</button>
           <button className="bg-blue-400 rounded-2xl w-16">artist</button>
